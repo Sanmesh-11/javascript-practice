77f75caeecd852f5292b7251042c4f32690066b4
Sanmesh-11 <sanmeshpatil2003@gmail.com>
2025-07-02 17:51:30 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=144323
+let accountEmail="Sam@google.com"
+var accountPassword="1234"
+accountCity="Pune"
+
+console.table(accountId,accountEmail,accountPassword);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+4) // we are using nodejs , not browser
+
+// number => 2 to power 53
+// bigint 
+// string =>""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+// object 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
